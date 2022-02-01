21ce843f39a15caa9a021459f4880ba1c4ff494c
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2022-01-31 22:39:00 -0600

    bank account controller index create routes

--- app/controllers/api/v1/bank_accounts_controller.rb
+++ app/controllers/api/v1/bank_accounts_controller.rb
@@ -1,2 +1,20 @@
 class Api::V1::BankAccountsController < ApplicationController
+    def index
+        bank_accounts = BankAccount.all
+        render json: bank_accounts
+    end
+    def create 
+        bank_account = BankAccount.new(bank_account_params)
+        if bank_account.save
+            render json: bank_account, status: :accepted
+        else
+            render json: { errors: bank_account.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def bank_account_params
+        params.require(:bank_account).permit(:name, :account_type, :starting_balance, :low_balance_alert)
+    end
 end
--- db/seeds/001_bank_accounts.rb
+++ db/seeds/001_bank_accounts.rb
@@ -1,4 +1,4 @@
 BankAccount.find_or_create_by(name: 'Chase', account_type: 'Checking', starting_balance: 5000, low_balance_alert: 500)
 BankAccount.find_or_create_by(name: 'Wells Fargo', account_type: 'Savings', starting_balance: 10000, low_balance_alert: 2500)
 BankAccount.find_or_create_by(name: 'Credit', account_type: 'Credit', starting_balance: 20000, low_balance_alert: 5000)
-BankAccount.destroy_all
+# BankAccount.destroy_all
