cefb5d11fc98ea28ed171f4b20d8da3e9b609b6c
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2022-02-03 10:40:48 -0600

    cors enabled

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do 
     namespace :v1 do
-      resources :bank_accounts, only: [:index]
-      resources :transactions, only: [:index]
+      resources :bank_accounts, only: [:index, :create]
+      resources :transactions, only: [:index, :create]
       end
     end
   end
